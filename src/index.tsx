ea24293d58507fbe98ea1eed602a4bd4db9e68fe
octopus-coder <>
2021-12-18 21:54:23 -0800

    Verified Pixel

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import AppProvider from "./AppContext";
 config();
 // require("dotenv").config();
 const PIXEL_ID = process.env.REACT_APP_PIXEL_ID ?? "";
+console.log(PIXEL_ID);
 
 ReactPixel.fbq("set", "autoConfig", false, PIXEL_ID);
 ReactPixel.init(PIXEL_ID);
