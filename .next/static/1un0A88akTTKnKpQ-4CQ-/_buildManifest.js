6f54649cea6b2b336176686fda688bb661598cb4
octopus-coder <>
2021-12-23 21:39:39 -0800

    Removed .next dir

--- .next/static/1un0A88akTTKnKpQ-4CQ-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-dd64cf026e8fd50c.js","static/chunks/600-1e4b4396acc2fd3d.js","static/chunks/pages/index-a8ae270b0f051e26.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
