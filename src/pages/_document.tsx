54520aeacf70b2960f0c17dca8ec3ea1b6532c5d
Luis Enrique <>
2022-06-30 00:25:25 -0700

    Corrected Pixel Init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,7 +46,7 @@ export default class MyDocument extends Document {
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID});
+      fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID}');
       fbq('track', 'PageView');`,
             }}
           />
